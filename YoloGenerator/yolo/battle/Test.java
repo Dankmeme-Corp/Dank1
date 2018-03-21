9f137cea89071adab571d8128f8814c8eee04703
psypasta <robin.van.manen@hotmail.com>
2018-03-22 00:41:08 +0100

    whitespaces gone

--- YoloGenerator/yolo/battle/Test.java
+++ YoloGenerator/yolo/battle/Test.java
@@ -9,17 +9,14 @@ public class Test {
 	public static void main(String[] args) {
 		Unit ua;
 		Unit ub;
-		 
 
 		ua = new Archer("Archer");
 		ub = new Swordman("Sworder");
 
-		
 		new Eval(ua, ub);
-		
+
 		ua = new Archer("Archer");
 		ub = new Swordman("Sworder");
-		
 
 
 		new Eval(ub, ua);
